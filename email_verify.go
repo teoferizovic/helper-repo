0d6eee6ef9bc8cc7013bc8f41081ad6ed2375d17
teoferizovic <ferizovic.teo@gmail.com>
2018-10-05 18:34:50 +0200

    New repo for helper fuctions to have on live server.

---
+++ email_verify.go
@@ -0,0 +1,13 @@
+package helper_repo
+
+import (
+	"regexp"
+)
+
+func VerifyEmail(email string) bool {
+
+	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	value := re.MatchString(email)
+	return value
+
+}
