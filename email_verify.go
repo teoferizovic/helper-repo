baa4fa485e08cb571105fd01af0b672cedee29e6
teoferizovic <ferizovic.teo@gmail.com>
2018-10-05 22:02:34 +0200

    Added gitignore file and return error on function.

--- email_verify.go
+++ email_verify.go
@@ -4,10 +4,10 @@ import (
 	"regexp"
 )
 
-func VerifyEmail(email string) bool {
+func VerifyEmail(email string) (bool,error) {
 
 	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 	value := re.MatchString(email)
-	return value
+	return value,nil
 
 }
